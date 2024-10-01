3fcb1c089f4684f21f0aad3f64fe0f67a7c29805
armandotruglio <armando.truglio1@gmail.com>
2024-10-01 17:03:31 +0200

    Completed sommaInteri function in merenda.js

--- merenda.js
+++ merenda.js
@@ -1,3 +1,19 @@
 function sommaInteri(N) {
     // A voi l'onore!
+
+    if (N === '' || N < 0) {
+        return 'Invalid input';
+    }
+
+    let somma = 0;
+    let contatore = 0;
+
+    while (somma <= N) {
+        contatore++;
+        console.log(contatore, 'CONTATORE');
+        somma += contatore;
+        console.log(somma, 'SOMMA')
+    }
+
+    return contatore - 1;
 }
